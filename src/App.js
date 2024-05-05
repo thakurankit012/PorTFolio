a6212f4c73a8c3dc86560e75b2bc18d21461021e
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-05-05 15:21:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
@@ -8,23 +9,26 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Mode from './components/Mode';
 
-
 function App() {
+  const [selectedLanguage, setSelectedLanguage] = useState('en'); // Default language is English
+
+  // Function to handle language change
+  const changeLanguage = (langCode) => {
+    setSelectedLanguage(langCode);
+  };
+
   return (
     <>
-      <Header />
-      <Hero />
-      <About />
-      <Projects />
-      <Blog />
-      <Contact />
-      <Footer />
+      <Header changeLanguage={changeLanguage} />
+      <Hero selectedLanguage={selectedLanguage} />
+      <About selectedLanguage={selectedLanguage} />
+      <Projects selectedLanguage={selectedLanguage} />
+      <Blog selectedLanguage={selectedLanguage} />
+      <Contact selectedLanguage={selectedLanguage} />
+      <Footer selectedLanguage={selectedLanguage} />
       <Mode />
-     
     </>
   );
 }
 
-    
-  
 export default App;
