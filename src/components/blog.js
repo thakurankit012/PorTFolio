b53bcb6837ad60bfa616a478366d68649de4a15a
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-05-05 15:20:32 +0530

    Update blog.js

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,42 +1,53 @@
 import React from 'react';
 
+const Blog = ({ selectedLanguage }) => {
+  // Define content based on language
+  const content = {
+    en: {
+      title: 'Certificates & Diplomas',
+      paragraph: 'Adding soon...',
+    },
+    es: {
+      title: 'Certificados y Diplomas',
+      paragraph: 'Próximamente...',
+    },
+    // Add more languages and translations as needed
+  };
 
-const Blog = () => {
   return (
     <section className='bg-secondary text-secondary py-16 md:py-32 px-8 md:px-32' id='blog'>
-      {}
       <div className='container mx-auto grid pt-16 md:grid-cols-2 items-center md:justify-between' id='cert_dipl'>
         <div className='about-info mb-5'>
           <h2 className='text-4xl font-bold mb-5 border-b-[5px] w-[220px] border-accent pb-2'>
-            Certificates <br />& Diplomas
+            {content[selectedLanguage].title}
           </h2>
-          <p className='pb-5' >adding soon.....</p>
+          <p className='pb-5'>{content[selectedLanguage].paragraph}</p>
         </div>
         <div className='about-img'></div>
       </div>
       <div className='projects container mx-auto grid md:grid-cols-3 gap-10 text-center	'>
         <div className=''>
-          <a href='d' target='_blank' rel='noreferrer'>
+          <a href='./#about' target='_blank' rel='noreferrer'>
             {/* <img  alt='img' className='object-contain h-80 cursor-pointer' /> */}
           </a>
         </div>
         <div className=''>
-          <a href='.//' target='_blank' rel='noreferrer'>
+          <a href='./#about' target='_blank' rel='noreferrer'>
             {/* <img src='' alt='img' className='object-contain h-80 cursor-pointer' /> */}
           </a>
         </div>
         <div className=''>
-          <a href='.//' target='_blank' rel='noreferrer'>
+          <a href='./#about' target='_blank' rel='noreferrer'>
             {/* <img  alt='img' className='object-contain h-80 cursor-pointer' /> */}
           </a>
         </div>
         <div className=''>
-          <a href='//' target='_blank' rel='noreferrer'>
+          <a href='./#about' target='_blank' rel='noreferrer'>
             {/* <img  alt='img' className='object-contain h-80 cursor-pointer' /> */}
           </a>
         </div>
-        <div className=''>
-          <a href='w' target='_blank' rel='noreferrer'>
+        <div className='#about'>
+          <a href='./#about' target='_blank' rel='noreferrer'>
             {/* <img  alt='img' className='object-contain h-80 cursor-pointer' /> */}
           </a>
         </div>
