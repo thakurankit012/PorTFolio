1b768798074f186bb78a5a158a2aa51660090fe9
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-05-05 15:39:17 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,30 +21,29 @@ const Header = ({ changeLanguage }) => {
       <nav className='hidden md:block'>
         <ul className='flex'>
           <li>
-            <a href='/#about'>About</a>
+            <a href='/#about'>{changeLanguage === 'es' ? 'Acerca de' : 'About'}</a>
           </li>
           <li>
-            <a href='/#projects'>Projects</a>
+            <a href='/#projects'>{changeLanguage === 'es' ? 'Proyectos' : 'Projects'}</a>
           </li>
           <li>
-            <a href='/#cert_dipl'>Certificates & Diplomas</a>
+            <a href='/#cert_dipl'>{changeLanguage === 'es' ? 'Certificados & Diplomas' : 'Certificates & Diplomas'}</a>
           </li>
           <li>
-            <a href='/#contact'>Contact</a>
+            <a href='/#contact'>{changeLanguage === 'es' ? 'Contacto' : 'Contact'}</a>
           </li>
           <li>
             <a href='https://drive.google.com/file/d/1dg0M4MqwBCNgvuIU-jTUmvejwWhbs7m3/view?usp=sharing' target='_blank' rel='noreferrer'>
-              Resume
+              {changeLanguage === 'es' ? 'Currículum' : 'Resume'}
             </a>
           </li>
           {/* Language Selection */}
           <li>
-            <button onClick={() => changeLanguage('en')} className='mr-2'>English</button>
+            <button onClick={() => changeLanguage('en')} className='mr-2'>{changeLanguage === 'es' ? 'Inglés' : 'English'}</button>
           </li>
           <li>
-            <button onClick={() => changeLanguage('es')} className='mr-2'>Spanish</button>
+            <button onClick={() => changeLanguage('es')} className='mr-2'>{changeLanguage === 'es' ? 'Español' : 'Spanish'}</button>
           </li>
-          
           {/* Add more buttons for other languages as needed */}
         </ul>
       </nav>
@@ -54,35 +53,35 @@ const Header = ({ changeLanguage }) => {
         <ul className='flex flex-col py-10 text-5xl'>
           <li>
             <a href='/#about' onClick={handleToggle}>
-              About
+              {changeLanguage === 'es' ? 'Acerca de' : 'About'}
             </a>
           </li>
           <li>
             <a href='/#projects' onClick={handleToggle}>
-              Projects
+              {changeLanguage === 'es' ? 'Proyectos' : 'Projects'}
             </a>
           </li>
           <li>
             <a href='/#cert_dipl' onClick={handleToggle}>
-              Certificates & Diplomas
+              {changeLanguage === 'es' ? 'Certificados & Diplomas' : 'Certificates & Diplomas'}
             </a>
           </li>
           <li>
             <a href='/#contact' onClick={handleToggle}>
-              Contact
+              {changeLanguage === 'es' ? 'Contacto' : 'Contact'}
             </a>
           </li>
           <li>
             <a href='https://drive.google.com/file/d/1dg0M4MqwBCNgvuIU-jTUmvejwWhbs7m3/view?usp=sharing' target='_blank' rel='noreferrer' onClick={handleToggle}>
-              Resume
+              {changeLanguage === 'es' ? 'Currículum' : 'Resume'}
             </a>
           </li>
           {/* Language Selection */}
           <li>
-            <a onClick={() => changeLanguage('en')} className='mr-2'>English</a>
+            <button onClick={() => changeLanguage('en')} className='mr-2'>{changeLanguage === 'es' ? 'Inglés' : 'English'}</button>
           </li>
           <li>
-            <a onClick={() => changeLanguage('es')} className='mr-2'>Spanish</a>
+            <button onClick={() => changeLanguage('es')} className='mr-2'>{changeLanguage === 'es' ? 'Español' : 'Spanish'}</button>
           </li>
           {/* Add more buttons for other languages as needed */}
         </ul>
@@ -94,8 +93,8 @@ const Header = ({ changeLanguage }) => {
           {!toggle ? <AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
         </button>
         <div className={`absolute top-14 right-4 z-20 bg-primary rounded-lg shadow-md py-2 px-4 ${toggle ? 'block' : 'hidden'}`}>
-          <button onClick={() => changeLanguage('en')} className='block w-full py-2 text-left'>English</button>
-          <button onClick={() => changeLanguage('es')} className='block w-full py-2 text-left'>Spanish</button>
+          <button onClick={() => changeLanguage('en')} className='block w-full py-2 text-left'>{changeLanguage === 'es' ? 'Inglés' : 'English'}</button>
+          <button onClick={() => changeLanguage('es')} className='block w-full py-2 text-left'>{changeLanguage === 'es' ? 'Español' : 'Spanish'}</button>
           {/* Add more buttons for other languages as needed */}
         </div>
       </div>
