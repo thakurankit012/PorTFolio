bae7c66edb432e0ee742f45eaca381f27f3ef9d3
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-06-15 18:30:25 +0530

    Update blog.js

--- src/components/blog.js
+++ src/components/blog.js
@@ -29,7 +29,7 @@ const Blog = ({ selectedLanguage }) => {
       <div className='projects container mx-auto grid md:grid-cols-3 gap-10 text-center	'>
         <div className=''>
           <a href='https://ibb.co/3s0k69g' target='_blank' rel='noreferrer'>
-            <img  alt='img' src={certificate} className='object-contain h-80 cursor-pointer' />
+            <img  alt='img' src={} className='object-contain h-80 cursor-pointer' />
           </a>
         </div>
         <div className=''>
